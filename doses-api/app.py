1659bdc4580a181c680e5813a2fd2c9e66d92b40
Jessica Ellis <me@datawitch.tech>
2023-07-31 07:48:03 -0500

    add app skeleton

---
+++ doses-api/app.py
@@ -0,0 +1,26 @@
+import sqlite3
+from flask import Flask
+
+app = Flask(__name__)
+
+def dict_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
+
+def get_db_connection():
+    conn = sqlite3.connect('data.db')
+    conn.row_factory = dict_factory
+    return conn
+
+@app.get("/healthz")
+def healthz():
+    return {'status': 200}
+
+def init_db():
+    cur = get_db_connection().cursor()
+    with open("schema.sql") as f:
+        cur.executescript(f.read())
+
+if __name__ == "__main__":
+    init_db()
+    app.run(host='0.0.0.0')
